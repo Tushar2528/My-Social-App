0affd79b305022345d6af5f2693db41a3d27f3c2
Tushar Bahuguna <tusharbahuguna2528@gmail.com>
2023-03-22 13:35:48 +0530

    Install view engine ejs and set it up in the main index.js file

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 8000;
 
 // Use express router
-app.use('/', require('./routes'))
+app.use('/', require('./routes'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 
 
